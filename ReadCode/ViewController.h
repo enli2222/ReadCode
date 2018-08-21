280f6fa1fb75d496e3bcaa2b54b4cacdd3a72494
神格的枷锁 <zpjoe@zhoupengdemacbook.local>
2018-08-21 22:08:30 +0800

    Initial Commit

---
+++ ReadCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReadCode
+//
+//  Created by 周鹏 on 2018/8/21.
+//  Copyright © 2018年 zpjoe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ReadCode/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  ReadCode
+//
+//  Created by 周鹏 on 2018/8/21.
+//  Copyright © 2018年 zpjoe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor blueColor];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
